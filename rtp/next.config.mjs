d80ca092f85901a1b47a5f249155ab2e598d7664
luke <luke.taful@gmail.com>
2025-10-16 22:51:11 +0100

    adding basepath

--- rtp/next.config.mjs
+++ rtp/next.config.mjs
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  basePath: isProd ? '/ruthtaaffepoetry/' : '',
   assetPrefix: isProd ? '/ruthtaaffepoetry/' : '',
 }
 
