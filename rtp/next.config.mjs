27db90131cdf10df19ccca892988e7e1db4290c0
luke <luke.taful@gmail.com>
2025-10-16 22:07:02 +0100

    update assetprefix in package

--- rtp/next.config.mjs
+++ rtp/next.config.mjs
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  assetPrefix: isProd ? '/ruthtaaffepoetry/' : '',
+  assetPrefix: isProd ? '/rtp/' : '',
 }
 
 export default nextConfig
