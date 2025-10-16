3753c39dcde38a9080bdd65e89573aa8ae5ee011
luke <luke.taful@gmail.com>
2025-10-16 22:10:17 +0100

    renaming app to fit with repo

---
+++ ruthtaaffepoetry/next.config.mjs
@@ -0,0 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  assetPrefix: isProd ? '/ruthtaaffepoetry/' : '',
+}
+
+export default nextConfig
